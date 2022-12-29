38ec9ba3b03e882b1dd39985442546ce1b05ed16
Kareem-Shahhat <100204962+Kareem-Shahhat@users.noreply.github.com>
2022-12-29 02:16:49 +0200

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -11,7 +11,7 @@ hamburger.addEventListener('click', () => {
 document.addEventListener('scroll', () => {
 	var scroll_position = window.scrollY;
 	if (scroll_position > 250) {
-		header.style.backgroundImage = 'linear-gradient(181deg, #29323c 43%, #9097a0 152%)';
+		header.style.backgroundImage = 'linear-gradient(181deg, rgb(63, 11, 41) 43%, rgb(100 85 86) 152%)';
 		header.style.borderBottom='1px solid red' ;
 	} else {
 		header.style.backgroundImage = 'linear-gradient(181deg, rgb(41 50 60 / 34%) 43%, rgb(144 151 160 / 25%) 152%)';
@@ -26,16 +26,31 @@ menu_item.forEach((item) => {
 		mobile_menu.classList.toggle('active');
 	});
 });
-// background-image: linear-gradient(181deg, #29323c 43%, #9097a0 152%);
-// #29323c
-// var act = document.querySelector("#header .nav-list ul li");
 
-// document.addEventListener('scroll', () => {
-// 	var scroll_position = window.scrollY;
-// 	if (scroll_position >= 250) {
-// 		act.classList.toggle('activeh');
-// 	} 
-// 	else if (scroll_position >= 350){
-// 		console.log("hello");
-// 	}
-// });
+
+let tabs = document.querySelectorAll(".tabs_ul li");
+let tabsArray = Array.from(tabs);
+
+let devs_cont = document.querySelectorAll(".content_rev > div");
+let devs_contarray = Array.from(devs_cont);
+
+tabsArray.forEach((ele) => {
+    ele.addEventListener("click",function(e){
+        tabsArray.forEach((ele)=>{
+            ele.classList.remove("active_tab_rev");
+        });
+        e.currentTarget.classList.add("active_tab_rev");
+
+        devs_contarray.forEach((div)=>{
+            div.style.display="none";
+        });
+        console.log(e.currentTarget.dataset.cont);
+        document.querySelector(e.currentTarget.dataset.cont).style.display="block";
+    });
+});
+
+let projectNum = document.querySelectorAll(".tab_one .project-items");
+let prelm1 = document.getElementById("proj1").innerHTML=projectNum.length;
+
+let projectNum2 = document.querySelectorAll(".tab_two .project-items");
+let prelm2 = document.getElementById("proj2").innerHTML=projectNum2.length;
